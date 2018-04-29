9619b313d9a82b777ae8c68207114857f5e46867
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-04-29 22:05:50 +0530

    complete!!!
    copy and past the code form my local project Due to this i am getting error in my mainActivity .
    
    ^^^^^^   but the project working find ^^^

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,17 @@
 package com.londonappbrewery.magiceightball;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView make = (TextView )findViewById(R.id.makedecision);
+        final ImageView ballImage = (ImageView) findViewById(R.id.ballIImage);
+        final ImageView outputImage = (ImageView) findViewById(R.id.outputImage);
+        Button luck = (Button) findViewById(R.id.TryTheluck);
+
+        final int[] tryluck = {
+                R.drawable.no,
+                R.drawable.no1,
+                R.drawable.no2,
+                R.drawable.yes,
+                R.drawable.yes1,
+                R.drawable.no,
+                R.drawable.no1,
+                R.drawable.no2,
+                R.drawable.yes,
+                R.drawable.yes1,
+                R.drawable.no,
+                R.drawable.no1,
+                R.drawable.no2,
+                R.drawable.yes,
+                R.drawable.yes1,
+
+        };
+
+        luck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                final int nu = random.nextInt(12);
+                Log.d("no","no "+nu);
+                outputImage.setImageResource(tryluck[nu]);
+            }
+        });
+
+
+
+
     }
+
 }
